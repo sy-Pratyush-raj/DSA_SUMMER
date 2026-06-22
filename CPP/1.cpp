2685fb75ed281e710629b5841105585c42def19d
Pratyush Raj <rockpratyushraj@gmail.com>
2026-06-22 11:48:54 +0530

    Add initial configuration files for C++ development and example code

---
+++ CPP/1.cpp
@@ -0,0 +1,62 @@
+//                        Data type and variables
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int age;
+//     cout << age << endl;   
+   
+// }
+
+//                                  if/else
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int age, num;
+
+//     if(num>10 && age >18){
+//         cout<<"test will be there";
+//     }else{
+//         cout<<"test will not be there ";
+//     }
+    
+// }
+
+
+//                                 switch case
+
+// #include <iostream>
+// using namespace  std;
+
+// int main(){
+
+//     char grade='A';
+
+//     switch(grade){
+//         case 'A':
+//         cout<<"Excellent"<<endl;
+//         break;
+//         case 'B':
+//         cout<<"Good"<<endl;
+//         break;
+//         case 'C':
+//         cout<<"Average"<<endl;
+//         break;
+//         default:
+//         cout<<"Invalid"<<endl;
+//     }
+// }
+
+//                       Conditional (Ternary)
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10, y = 20;
+int ans = (x > y) ? x : y;
+cout << ans;
+}
+
