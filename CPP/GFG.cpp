d8e641810583d1251fad5c2bc724b1ba71079f21
Pratyush Raj <rockpratyushraj@gmail.com>
2026-06-25 11:49:17 +0530

    Add implementation for sorting an array of 0s, 1s, and 2s; add methods for removing duplicates and merging sorted arrays

--- CPP/GFG.cpp
+++ CPP/GFG.cpp
@@ -74,3 +74,14 @@
 //     }
 // };
 
+
+//                    Sort 0s, 1s and 2s
+
+
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        sort(arr.begin(),arr.end());
+    }
+};
--- CPP/leetcode.cpp
+++ CPP/leetcode.cpp
@@ -120,3 +120,56 @@
 //         return maxProfit;
 //     }
 // };
+
+//               26. Remove Duplicates from Sorted Array
+
+
+
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         if (nums.size() == 0) return 0;
+
+//         int k = 1;   
+
+//         for (int i = 1; i < nums.size(); i++) {
+//             if (nums[i] != nums[i - 1]) {
+//                 nums[k] = nums[i];
+//                 k++;
+//             }
+//         }
+//         return k;
+//     }
+// };
+
+
+//                       88. Merge Sorted Array
+
+
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         int i = m - 1;
+//         int j = n - 1;
+//         int k = m + n - 1;
+
+//         while (i >= 0 && j >= 0) {
+//             if (nums1[i] > nums2[j]) {
+//                 nums1[k] = nums1[i];
+//                 i--;
+//             } else {
+//                 nums1[k] = nums2[j];
+//                 j--;
+//             }
+//             k--;
+//         }
+
+//         while (j >= 0) {
+//             nums1[k] = nums2[j];
+//             j--;
+//             k--;
+//         }
+//     }
+// };
+
+
