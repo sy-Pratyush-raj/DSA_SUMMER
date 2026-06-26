00377fc9143781ae0276100c1fdbfd4e2c624170
Pratyush Raj <rockpratyushraj@gmail.com>
2026-06-26 10:57:26 +0530

    Add implementations for pair sum in a sorted and rotated array; add search range method for finding first and last positions of an element

--- CPP/GFG.cpp
+++ CPP/GFG.cpp
@@ -85,3 +85,41 @@
 //         sort(arr.begin(),arr.end());
 //     }
 // };
+
+
+
+//              Pair Sum in a Sorted and Rotated Array
+
+// class Solution {
+//   public:
+//     bool pairInSortedRotated(vector<int>& arr, int target) {
+//         // code here
+//         int pivot=0;
+//         int n = arr.size();
+        
+//         for(int i=0; i<n-1;i++){
+//             if(arr[i] > arr[i+1]){
+//                 pivot =i+1;
+//                 break;
+//             }
+//         }
+        
+//         int i = pivot;
+//         int j = (pivot -1 +n)%n;
+        
+        
+//         while (i != j){
+//             int sum = arr[i] + arr[j];
+//             if(sum == target) return true;
+            
+//             else if (sum<target){
+//                 i = (i+1)%n;
+//             }
+//             else {
+//                 j= (j-1+n)%n;
+//             }
+//         }
+//         return false;
+        
+//     }
+// };
--- CPP/leetcode.cpp
+++ CPP/leetcode.cpp
@@ -172,4 +172,42 @@
 //     }
 // };
 
+// 34. Find First and Last Position of Element in Sorted Array
 
+
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         int first = -1, last = -1;
+
+//         int l = 0, r = n - 1;
+//         while (l <= r) {
+//             int mid = (l + r) / 2;
+//             if (nums[mid] == target) {
+//                 first = mid;
+//                 r = mid - 1;
+//             } else if (nums[mid] < target) {
+//                 l = mid + 1;
+//             } else {
+//                 r = mid - 1;
+//             }
+//         }
+
+//         l = 0; r = n - 1;
+//         while (l <= r) {
+//             int mid = (l + r) / 2;
+//             if (nums[mid] == target) {
+//                 last = mid;
+//                 l = mid + 1;
+//             } else if (nums[mid] < target) {
+//                 l = mid + 1;
+//             } else {
+//                 r = mid - 1;
+//             }
+//         }
+
+//         return {first, last};
+//     }
+
+//  };
