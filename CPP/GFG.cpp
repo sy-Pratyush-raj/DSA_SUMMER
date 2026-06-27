d49be4c7744831020fc71192b9249dc16eb4e788
Pratyush Raj <rockpratyushraj@gmail.com>
2026-06-27 16:16:02 +0530

    Add implementation for finding the floor of a number in a sorted array; add reverse integer function

--- CPP/GFG.cpp
+++ CPP/GFG.cpp
@@ -123,3 +123,32 @@
         
 //     }
 // };
+
+
+//                  Floor in a Sorted Array
+
+
+class Solution {
+public:
+    int findFloor(vector<int>& arr, int x) {
+
+        int low = 0;
+        int high = arr.size() - 1;
+        int ans = -1;
+
+        while(low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if(arr[mid] <= x) {
+                ans = mid;          
+                low = mid + 1;      
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
