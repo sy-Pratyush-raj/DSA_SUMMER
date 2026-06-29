131d88a7ca8650d6e0fa9071b72d0dff9f092877
Pratyush Raj <rockpratyushraj@gmail.com>
2026-06-29 10:26:21 +0530

    Add implementation for max sum subarray of size K

--- CPP/GFG.cpp
+++ CPP/GFG.cpp
@@ -172,3 +172,28 @@
 //         }
 //     }
 // };
+
+
+// Max Sum Subarray of size K
+
+
+// class Solution {
+// public:
+//     int maxSubarraySum(vector<int>& arr, int k) {
+//         int n = arr.size();
+//         int windowSum = 0;
+//         int maxSum = 0;
+
+//         for (int i = 0; i < k; i++) {
+//             windowSum += arr[i];
+//         }
+//         maxSum = windowSum;
+
+//         for (int i = k; i < n; i++) {
+//             windowSum += arr[i];        
+//             windowSum -= arr[i - k];    
+//             maxSum = max(maxSum, windowSum);
+//         }
+//         return maxSum;
+//     }
+// };
