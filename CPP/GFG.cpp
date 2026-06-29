f7ae01e7846a0059358174827488d9584322780f
Pratyush Raj <rockpratyushraj@gmail.com>
2026-06-29 09:47:50 +0530

    Add implementations for reversing an array in groups and finding the maximum subarray

--- CPP/GFG.cpp
+++ CPP/GFG.cpp
@@ -152,3 +152,23 @@
 //         return ans;
 //     }
 // };
+
+//              Reverse array in groups
+
+// class Solution {
+//   public:
+//     void reverseInGroups(vector<int> &arr, int k) {
+//         int n = arr.size();
+
+//         for (int i = 0; i < n; i += k) {
+//             int start = i;
+//             int end = min(i + k - 1, n - 1);
+
+//             while (start < end) {
+//                 swap(arr[start], arr[end]);
+//                 start++;
+//                 end--;
+//             }
+//         }
+//     }
+// };
--- CPP/leetcode.cpp
+++ CPP/leetcode.cpp
@@ -233,3 +233,25 @@
 //         return rev;
 //     }
 // };
+
+//                       53. Maximum Subarray
+
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+
+//         int currSum =0;
+//         int maxSum =  nums[0];
+
+//         for(int i=0; i<nums.size(); i++){
+//             currSum += nums[i];
+
+//             if(currSum > maxSum) maxSum = currSum;
+
+//             if(currSum < 0) currSum =0;
+
+//         }
+//         return maxSum;   
+//     }
+// };
+
