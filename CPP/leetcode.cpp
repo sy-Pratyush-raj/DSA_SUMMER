6f69ee8156acb28fb11a31f48d9bc90943207438
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-02 10:30:37 +0530

    Add implementation for Is Subsequence problem

--- CPP/leetcode.cpp
+++ CPP/leetcode.cpp
@@ -308,3 +308,24 @@
 //         return area;
 //     }
 // };
+
+
+//                       392. Is Subsequence
+
+
+
+// class Solution {
+// public:
+//     bool isSubsequence(string s, string t) {
+//         int i = 0, j = 0;
+
+//         while (i < s.length() && j < t.length()) {
+//             if (s[i] == t[j]) {
+//                 i++;
+//             }
+//             j++;
+//         }
+
+//         return i == s.length();
+//     }
+// };
