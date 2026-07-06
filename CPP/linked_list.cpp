8e6800b1c1b74e99f6992fccd655e15e6d46db5b
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-06 09:37:18 +0530

    Refactor linked list implementation by uncommenting code and adding missing functions

--- CPP/linked_list.cpp
+++ CPP/linked_list.cpp
@@ -1,61 +1,89 @@
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
+ 
+ class Node{
+    public:
+    int data;
+    Node *next;
 
-// class Node{
-//     public:
-//     int data;
-//     Node *next;
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+ };
 
-//     Node(int val){
-//         data = val;
-//         next = NULL;
-//     }
-// };
-
-// void insertAtBeginning(Node* &head, int val) {
-//     Node* node = new Node(val);
-//     node->next = head;
-//     head = node;
-// }
-
-// void insertAtEnd(Node * &head , int val){
-//     Node* node = new Node(val);
-//     if(head == NULL){
-//         head = node;
-//         return;
-//     }
-    
-//     Node* temp = head;
-//     while(temp -> next != NULL){
-//         temp = temp->next;
-//     }
-//     temp->next = node;
-// }
-
-// int main(){
-//     Node* head = NULL;
-//     insertAtEnd(head,1);
-//     insertAtEnd(head,2);
-//     insertAtEnd(head,3);
-
-// }
+ void insertAtEnd(Node* &head, int val){
+    Node* node = new Node(val);
+    if(head == NULL){
+        head = node;
+        return;
+    }
+    Node* temp = head;
 
 
-// void printEvenNodes(Node* head) {
-//     Node* temp = head;
-//     int pos = 1;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = node;
+ }
 
-//     while (temp != NULL) {
-//         if (pos % 2 == 0) {
-//             cout << temp->data << " ";
-//         }
-//         temp = temp->next;
-//         pos++;
-//     }
-//     cout << endl;
-// }
+ void insertAtBeginning(Node* &head, int val){
+    Node* node = new Node(val);
+    node->next = head;
+    head = node;
+ }
+
+ void printList(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << " NULL";
+ }
+
+ void insertAtPosition(Node* &head, int pos, int val){
+    Node* node = new Node(val);
+
+    if(pos==1){
+        node->next = head;
+        head = node;
+        return;
+    }
+    Node* temp = head;
+    for(int i = 1; i < pos - 1 && temp != NULL; i++){
+        temp = temp ->next;
+    }
+    node->next = temp ->next;
+    temp->next = node;
+ }
+ void deleteATEnd(Node* &head){
+
+    if(head==NULL){
+        return ;
+    }
+    if(head -> next == NULL){
+        delete head;
+        head = NULL;
+        return ;
+    }
+
+    Node* temp = head;
+    while(temp -> next -> next != NULL){
+        temp = temp -> next;
+    }
+    delete temp->next;
+    temp->next =NULL;
+ }
+
+int main()
+{
+    Node* head = NULL;
+    insertAtEnd(head, 1);
+    insertAtEnd(head, 2);
+    insertAtEnd(head, 3);
+    insertAtBeginning(head, 5);
+    printList(head);
 
 
-
-
-
+}
