395f0f448fa98017b8e547637278d5f86d836377
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-07 09:14:09 +0530

    Add implementation for finding the middle node of a linked list

--- CPP/leetcode.cpp
+++ CPP/leetcode.cpp
@@ -330,5 +330,22 @@
 //     }
 // };
 
+                // 876. Middle of the Linked List
+
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+
+//         while (fast != NULL && fast->next != NULL) {
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+
+//         return slow;
+//     }
+// };
+
 
 
