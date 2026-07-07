d3f2ed1afd8ca6cc5084fe4439ca964817387e47
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-07 11:50:01 +0530

    Add implementations for Linked List Cycle detection and Odd Even Linked List rearrangement

--- CPP/leetcode.cpp
+++ CPP/leetcode.cpp
@@ -391,6 +391,62 @@
 
 
 
+//                            141. Linked List Cycle
+
+
+
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+
+
+//         while (fast != NULL && fast->next != NULL) {
+//             slow = slow->next;
+//             fast = fast->next->next;
+
+//             if (slow == fast) {
+//                 return true;
+//             }
+//         }
+
+//         return false;
+//     }
+// };
+
+//                 328. Odd Even Linked List
+
+
+// class Solution {
+// public:
+//     ListNode* oddEvenList(ListNode* head) {
+
+//         if (head == NULL || head->next == NULL) {
+//             return head;
+//         }
+
+//         ListNode* odd = head;
+//         ListNode* even = head->next;
+//         ListNode* evenHead = even;
+
+//         while (even != NULL && even->next != NULL) {
+//             odd->next = even->next;
+//             odd = odd->next;
+
+//             even->next = odd->next;
+//             even = even->next;
+//         }
+
+//         odd->next = evenHead;
+
+//         return head;
+//     }
+// };
+
+
+
 
 
 
