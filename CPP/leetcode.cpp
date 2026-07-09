fe2e28428b9acd439ccaa776bcbb31dc96171127
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-09 09:30:04 +0530

    Add implementation for partitioning a linked list

--- CPP/leetcode.cpp
+++ CPP/leetcode.cpp
@@ -509,6 +509,41 @@
 //     }
 // };
 
+//                       86. Partition List
+
+// class Solution {
+// public:
+//     ListNode* partition(ListNode* head, int x) {
+//         ListNode* left = new ListNode(-1);
+//         ListNode* right = new ListNode(-1);
+
+//         ListNode* lp = left;
+//         ListNode* rp = right;
+
+//         ListNode* temp = head;
+
+//         while(temp != NULL){
+//             if(temp->val < x){
+//                 left -> next = temp;
+//                 left = left -> next;
+//             }else{
+//                 right -> next = temp;
+//                 right = right-> next;
+//             }
+//             temp = temp ->next;
+//         }
+//         right -> next = NULL;
+//         left -> next = rp->next;
+
+//         return lp->next;
+//     }      
+// };
+
+
+
+
+
+
 
 
 
