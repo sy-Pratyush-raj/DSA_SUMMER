ebfbfdfe2618ddcd6f5c9faa4ad8d4a3d5bdf18e
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-13 09:23:24 +0530

    Uncomment the implementation of the isValid function for Valid Parentheses in leetcode.stack.cpp

---
+++ CPP/_stack__queue/leetcode.stack.cpp
@@ -0,0 +1,26 @@
+//                    20. Valid Parentheses
+
+// class Solution {
+//     public:
+//         bool isValid(string s) {
+//             stack<char>st;
+
+//             for(char ch : s){
+//                 if(ch == '(' || ch =='{' || ch == '['){
+//                     st.push(ch);
+//                 }else{
+//                     if(st.empty()) return false;
+
+//                     char top =st.top();
+//                     if(ch==')' && top =='('||
+//                     ch=='}' && top =='{'||
+//                     ch==']' && top=='['){
+//                         st.pop();
+//                     }else{
+//                         return false;
+//                     }
+//                 }
+//             }
+//             return st.empty();
+//         }
+// };
