b22c69a7c3ac52771e8277445e2dad15f5ad2854
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-14 11:31:43 +0530

    Comment out existing code in notation.cpp and gfg.stack.cpp for clarity; add nextGreaterElement function in leetcode.stack.cpp

--- CPP/_stack__queue/notation.cpp
+++ CPP/_stack__queue/notation.cpp
@@ -1,55 +1,55 @@
 //                                 prefix
 
-#include<iostream>
-#include<stack>
-using namespace std;
+// #include<iostream>
+// #include<stack>
+// using namespace std;
 
-int prefixEval(string s){
-    // create a stack
+// int prefixEval(string s){
+//     // create a stack
 
-    //traverse from right to left 
+//     //traverse from right to left 
 
-    //if s[i] is operand -> push in the stack
+//     //if s[i] is operand -> push in the stack
 
-    // else if it is operator -> create two variable to store two elements,
-    // pop  them and evaluate with operator, them push the resulant back in stack
+//     // else if it is operator -> create two variable to store two elements,
+//     // pop  them and evaluate with operator, them push the resulant back in stack
 
-    // return the last remaining elements from stack -> it is the ans 
+//     // return the last remaining elements from stack -> it is the ans 
 
-    stack<int > st;
-    for(int i = s.length()-1; i>=0;i--){
-        if(s[i] >= '0' && s[i]<= '9' ){
-            st.push(s[i] -'0');
-        }else{
-            int operand1 = st.top();
-            st.pop();
-            int operand2 = st.top();
-            st.pop();
+//     stack<int > st;
+//     for(int i = s.length()-1; i>=0;i--){
+//         if(s[i] >= '0' && s[i]<= '9' ){
+//             st.push(s[i] -'0');
+//         }else{
+//             int operand1 = st.top();
+//             st.pop();
+//             int operand2 = st.top();
+//             st.pop();
 
-            switch (s[i])
-            {
-            case '+':
-                st.push(operand1 - operand2);
-                break;
+//             switch (s[i])
+//             {
+//             case '+':
+//                 st.push(operand1 - operand2);
+//                 break;
             
-            case '*':
-                st.push(operand1 * operand2);
-                break;
+//             case '*':
+//                 st.push(operand1 * operand2);
+//                 break;
 
-            case '/':
-                st.push(operand1 / operand2);
-                break;
+//             case '/':
+//                 st.push(operand1 / operand2);
+//                 break;
 
-            case '^':
-                st.push(operand1 ^ operand2);
-                break;
+//             case '^':
+//                 st.push(operand1 ^ operand2);
+//                 break;
             
             
-            }
-        }
-    }
-    return st.top();
-}
-int main(){
-    string s = "-+7*45+20";
-}
+//             }
+//         }
+//     }
+//     return st.top();
+// }
+// int main(){
+//     string s = "-+7*45+20";
+// }
