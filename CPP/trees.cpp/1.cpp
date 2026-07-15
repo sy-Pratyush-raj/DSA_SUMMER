daf5853497645a66621d403507a2cc863c8d35b6
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-15 11:37:42 +0530

    Add Node class implementation in trees.cpp

---
+++ CPP/trees.cpp/1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value){
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
