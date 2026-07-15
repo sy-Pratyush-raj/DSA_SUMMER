705930516988a5ab61e326801d5a14bd6f4d2482
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-15 09:53:21 +0530

    Add asteroidCollision function to leetcode.stack.cpp for handling asteroid collisions

--- CPP/_stack__queue/leetcode.stack.cpp
+++ CPP/_stack__queue/leetcode.stack.cpp
@@ -123,5 +123,47 @@
 //                          735. Asteroid Collision
 
 
+// class Solution {
+// public:
+//     vector<int> asteroidCollision(vector<int>& asteroids) {
 
+//         stack<int> st;
+
+//         for (int i : asteroids) {
+
+//             bool destroyed = false;
+
+//             while (!st.empty() && st.top() > 0 && i < 0) {
+
+//                 int topVal = st.top();
+
+//                 if (abs(topVal) < abs(i)) {
+//                     st.pop();
+//                 }
+//                 else if (abs(topVal) > abs(i)) {
+//                     destroyed = true;
+//                     break;
+//                 }
+//                 else {
+//                     st.pop();
+//                     destroyed = true;
+//                     break;
+//                 }
+//             }
+
+//             if (!destroyed) {
+//                 st.push(i);
+//             }
+//         }
+
+//         vector<int> ans(st.size());
+
+//         for (int i = st.size() - 1; i >= 0; i--) {
+//             ans[i] = st.top();
+//             st.pop();
+//         }
+
+//         return ans;
+//     }
+// };
 
