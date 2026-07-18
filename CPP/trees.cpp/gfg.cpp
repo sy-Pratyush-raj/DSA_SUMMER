5fc030a02f000fd733e24b7a4f17b74f8b4c68b3
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-18 11:00:47 +0530

    Implement boundary traversal and K distance from root functions in gfg.cpp

--- CPP/trees.cpp/gfg.cpp
+++ CPP/trees.cpp/gfg.cpp
@@ -68,3 +68,91 @@ class Solution {
 
 //                        Tree Boundary Traversal
 
+
+class Solution {
+  public:
+
+    void traverseLeft(Node *root, vector<int> &ans){
+        if(root == NULL || (root->left == NULL && root->right == NULL))
+            return;
+
+        ans.push_back(root->data);
+
+        if(root->left)
+            traverseLeft(root->left, ans);
+        else
+            traverseLeft(root->right, ans);
+    }
+
+    void traverseLeaf(Node *root, vector<int> &ans){
+        if(root == NULL)
+            return;
+
+        if(root->left == NULL && root->right == NULL){
+            ans.push_back(root->data);
+            return;
+        }
+
+        traverseLeaf(root->left, ans);
+        traverseLeaf(root->right, ans);
+    }
+
+    void traverseRight(Node *root, vector<int> &ans){
+        if(root == NULL || (root->left == NULL && root->right == NULL))
+            return;
+
+        if(root->right)
+            traverseRight(root->right, ans);
+        else
+            traverseRight(root->left, ans);
+
+        ans.push_back(root->data);
+    }
+
+    vector<int> boundaryTraversal(Node *root) {
+
+        vector<int> ans;
+
+        if(root == NULL)
+            return ans;
+
+        // Add root only if it is not a leaf
+        if(!(root->left == NULL && root->right == NULL))
+            ans.push_back(root->data);
+
+        traverseLeft(root->left, ans);
+        traverseLeaf(root, ans);
+        traverseRight(root->right, ans);
+
+        return ans;
+    }
+};
+
+//                 K distance from root
+
+class Solution {
+  public:
+  
+    void traverse(Node* root, int k, vector<int> &ans){
+        
+        if(root == NULL)
+            return;
+        
+        if(k == 0){
+            ans.push_back(root->data);
+        }
+        
+        traverse(root->left, k - 1, ans);
+        traverse(root->right, k - 1, ans);
+    }
+  
+    vector<int> kdistance(int k, Node *root) {
+        
+        vector<int> ans;
+        
+        traverse(root, k, ans);
+        
+        return ans;
+    }
+};
+
