8a337ed29d62716cd70e433e05a10f37d7b3eba7
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-18 11:39:07 +0530

    Implement searchBST function for binary search tree in leetcode.cpp

--- CPP/trees.cpp/leetcode.cpp
+++ CPP/trees.cpp/leetcode.cpp
@@ -185,3 +185,20 @@ public:
         
      }
 };
+
+//               700. Search in a Binary Search Tree
+
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL)
+        return NULL;
+        if(root->val==val)
+        return root;
+        if(root->val<val)
+        return searchBST(root->right,val);
+        return searchBST(root->left, val);
+        
+    }
+};
