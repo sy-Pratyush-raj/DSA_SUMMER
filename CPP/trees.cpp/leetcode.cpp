895e116f6c73b9807ef7d4f10acc60831058b019
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-18 11:25:37 +0530

    Implement lowest common ancestor function for binary tree in leetcode.cpp

--- CPP/trees.cpp/leetcode.cpp
+++ CPP/trees.cpp/leetcode.cpp
@@ -165,3 +165,23 @@ public:
     }
 };
 
+//           236. Lowest Common Ancestor of a Binary Tree
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL) return NULL;
+
+        if(root== p || root  ==q) return root;
+        TreeNode* leftLCA = lowestCommonAncestor(root->left, p,q);
+        TreeNode* rightLCA = lowestCommonAncestor(root->right, p,q);
+
+        if(leftLCA && rightLCA){
+            return root;
+        }
+        else if(leftLCA !=NULL) return leftLCA;
+         else return rightLCA;   
+        
+     }
+};
