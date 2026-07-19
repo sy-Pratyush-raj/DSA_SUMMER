d92e177ee84fc58d5932f49264c9efbaea10b5f8
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-20 00:36:06 +0530

    Implement range sum function for binary search tree in leetcode.cpp

--- CPP/trees.cpp/leetcode.cpp
+++ CPP/trees.cpp/leetcode.cpp
@@ -202,3 +202,23 @@ public:
         
     }
 };
+
+//                    938. Range Sum of BST
+
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(!root) return 0;
+
+        if(root->val < low)
+            return rangeSumBST(root->right, low, high);
+
+        if(root->val > high)
+            return rangeSumBST(root->left, low, high);
+
+        return root->val 
+             + rangeSumBST(root->left, low, high)
+             + rangeSumBST(root->right, low, high);
+    }
+};
