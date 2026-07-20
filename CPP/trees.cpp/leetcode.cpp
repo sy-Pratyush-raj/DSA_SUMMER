d70ffe0dcd7c9093a1e7abc38e8a1739bcd35336
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-20 09:55:45 +0530

    Implement findTarget function for Two Sum IV in a BST in leetcode.cpp

--- CPP/trees.cpp/leetcode.cpp
+++ CPP/trees.cpp/leetcode.cpp
@@ -262,3 +262,46 @@ public:
         return root;
     }
 };
+
+//                653. Two Sum IV - Input is a BST
+
+
+class Solution {
+public:
+
+    void inorder(TreeNode* root, vector<int>& nums){
+
+        if(root == NULL)
+            return;
+
+        inorder(root->left, nums);
+        nums.push_back(root->val);
+        inorder(root->right, nums);
+    }
+
+    bool findTarget(TreeNode* root, int k) {
+
+        vector<int> nums;
+
+        inorder(root, nums);
+
+        int i = 0;
+        int j = nums.size() - 1;
+
+        while(i < j){
+
+            int sum = nums[i] + nums[j];
+
+            if(sum == k)
+                return true;
+
+            else if(sum < k)
+                i++;
+
+            else
+                j--;
+        }
+
+        return false;
+    }
+};
