7287f19843146a26a1e92401503d3d237c1a3eeb
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-20 09:29:08 +0530

    Implement sorted array to binary search tree conversion in leetcode.cpp

--- CPP/trees.cpp/leetcode.cpp
+++ CPP/trees.cpp/leetcode.cpp
@@ -222,3 +222,27 @@ public:
              + rangeSumBST(root->right, low, high);
     }
 };
+
+
+
+
+         // 108. Convert Sorted Array to Binary Search Tree
+
+class Solution {
+public:
+    TreeNode* build(vector<int>& nums, int l, int r){
+        if(l > r) return NULL;
+
+        int mid = l + (r - l) / 2;
+
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = build(nums, l, mid - 1);
+        root->right = build(nums, mid + 1, r);
+
+        return root;
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return build(nums, 0, nums.size() - 1);
+    }
+};
