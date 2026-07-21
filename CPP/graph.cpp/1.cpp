7c8aa6b7c4776ed8c974a1c7a628b5578a7d5e69
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-21 11:33:03 +0530

    Add initial graph implementation files with basic structure and input handling

---
+++ CPP/graph.cpp/1.cpp
@@ -0,0 +1,4 @@
+/*
+A GRAPH G = (V,E) consist of :
+V-> set of vertices 
+*/
---
+++ CPP/graph.cpp/2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n ; //numbers of vertices
+    int m ; // numbers of edges
+
+    cout<<"Enter the numbers of vertices: " <<endl;
+    cin>>n;
+    cout<<"Enter the numbers of edges : " <<endl;
+    cin>>m;
+
+    unordered_map<int, list<int>> adjList;
+    cout<< "Enter edges(u,v): " << endl;
+
+    for(int i =0; i<n; i++){
+        int u,v;
+        cin>>u>>v;
+
+        adj
+    }
+
+}
