ccd14f655c197d2c9c6a66110fe6845faedb8a1d
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-22 10:15:22 +0530

    Implement DFS algorithm in gfg.cpp

--- CPP/graph.cpp/gfg.cpp
+++ CPP/graph.cpp/gfg.cpp
@@ -34,4 +34,27 @@ class Solution {
 
 //                                 DFS of Graph
 
+class Solution {
+public:
+    void dfsHelper(int node, vector<vector<int>>& adj,
+                   vector<bool>& visited, vector<int>& result) {
+        visited[node] = true;
+        result.push_back(node);
 
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                dfsHelper(neighbor, adj, visited, result);
+            }
+        }
+    }
+
+    vector<int> dfs(vector<vector<int>>& adj) {
+        int V = adj.size();
+        vector<bool> visited(V, false);
+        vector<int> result;
+
+        dfsHelper(0, adj, visited, result);
+
+        return result;
+    }
+};
