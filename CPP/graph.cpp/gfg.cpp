b4720c322edd8949372fd33c5d09eb72959ca6b9
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-22 11:01:09 +0530

    Add cycle detection in undirected graph and ensure proper formatting in leetcode.cpp

--- CPP/graph.cpp/gfg.cpp
+++ CPP/graph.cpp/gfg.cpp
@@ -58,3 +58,44 @@ public:
         return result;
     }
 };
+
+
+//                       Undirected Graph Cycle
+
+class Solution {
+public:
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(V);
+        
+        for(auto &e : edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
+        vector<int> vis(V, 0);
+
+        for(int i = 0; i < V; i++){
+            if(!vis[i]){
+                queue<pair<int,int>> q; 
+                q.push({i, -1});
+                vis[i] = 1;
+
+                while(!q.empty()){
+                    auto [node, parent] = q.front();
+                    q.pop();
+
+                    for(int nei : adj[node]){
+                        if(!vis[nei]){
+                            vis[nei] = 1;
+                            q.push({nei, node});
+                        }
+                        else if(nei != parent){
+                            return true;  
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
