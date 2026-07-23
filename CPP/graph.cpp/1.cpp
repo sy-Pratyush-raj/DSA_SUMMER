689c9ec5101926e2af61323a0592f062b4931c88
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-23 09:22:38 +0530

    Update graph documentation with detailed descriptions of vertices, edges, and graph types

--- CPP/graph.cpp/1.cpp
+++ CPP/graph.cpp/1.cpp
@@ -1,4 +1,20 @@
 /*
-A GRAPH G = (V,E) consist of :
-V-> set of vertices 
+    A graph G =(V, E) consist of :
+    V -> Set of Vertices(nodes) -> An individual objet in Graph
+    E -> Set of edges connecting pairs of vertices 
+
+    - Directed Graph -> edges have direction
+    - Undirected -> edges have no direction
+    - Weighted -> edges have values (weight/ cost/distance)
+    - Unweighted
+
+    Path ->a sequence of vertices connected edges
+
+    -Cycle -> a path that start and end at the same vertex
+
+     Degree -> in undirected graphs -> number of edges connected to a vertex
+     in directed graphs ->
+        - in degree -> incoming edges
+        - outdegree -> outgoing edges 
+
 */
