72d455cda3d96818a2d26161883d70336c1edf43
Pratyush Raj <rockpratyushraj@gmail.com>
2026-07-23 10:18:42 +0530

    Implement cycle detection for directed graphs in gfg.cpp

--- CPP/graph.cpp/gfg.cpp
+++ CPP/graph.cpp/gfg.cpp
@@ -99,3 +99,52 @@ public:
         return false;
     }
 };
+
+
+//                    Directed Graph Cycle
+
+
+
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>>& adj,
+             vector<int>& vis, vector<int>& pathVis) {
+
+        vis[node] = 1;
+        pathVis[node] = 1;
+
+        for(int nei : adj[node]) {
+
+            if(!vis[nei]) {
+                if(dfs(nei, adj, vis, pathVis))
+                    return true;
+            }
+            else if(pathVis[nei]) {
+                return true;   
+            }
+        }
+
+        pathVis[node] = 0;   
+        return false;
+    }
+
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+
+        vector<vector<int>> adj(V);
+
+        for(auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+        }
+
+        vector<int> vis(V, 0), pathVis(V, 0);
+
+        for(int i = 0; i < V; i++) {
+            if(!vis[i]) {
+                if(dfs(i, adj, vis, pathVis))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};
